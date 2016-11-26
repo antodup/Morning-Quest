0665e3471f46350192e435a34d85f07d23191b18
Ubuntu <anthony_dupre@live.fr>
2016-11-26 13:25:32 -0800

    Accueil OK

--- morning-quest-js.js
+++ morning-quest-js.js
@@ -1,9 +1,16 @@
 var play = document.getElementById('play'),
 	start = document.getElementById('start'),
 	home1 = document.getElementById('home1'),
-	home2 = document.getElementById('home2');
+	home2 = document.getElementById('home2'),
+	input = document.getElementById('name');
 
-function click() {  
+var click = function() {  
+
+	if (input.value==""){
+	alert ('Tu dois remplir le champs !');
+
+	} else {
+	
 	var animate = document.getElementsByClassName('animate');
 	start.classList.add ('animate');
 
@@ -12,7 +19,9 @@ function click() {
 
 	var open2 = document.getElementsByClassName('open2'); 
 	home2.classList.add('open2');
+	}
 }
 
 play.addEventListener('click',click) ;
+
 
