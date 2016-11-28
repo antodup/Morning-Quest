7f1c40f383d045dee96f1ffafe0ef2cbc43ce713
Thibault GERMAIN <thibaultgermain@MacBook-Pro-de-Thibault.local>
2016-11-28 14:42:15 +0100

    push

--- morning-quest-js.js
+++ morning-quest-js.js
@@ -4,6 +4,7 @@ var play = document.getElementById('play'),
 	home1 = document.getElementById('home1'),
 	home2 = document.getElementById('home2'),
 	input = document.getElementById('name'),
+    interval,
 
     counter = document.getElementById('counter'),
     go = 3,
@@ -15,6 +16,33 @@ var play = document.getElementById('play'),
     pastabox = document.getElementById('pastabox');
 
     
+function countdown( elementName, minutes, seconds )
+{
+    var element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits( n )
+    {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer()
+    {
+        msLeft = endTime - (+new Date);
+        if ( msLeft < 1000 ) {
+            element.innerHTML = "countdown's over!";
+        } else {
+            time = new Date( msLeft );
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+        }
+    }
+
+    element = document.getElementById( elementName );
+    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+    updateTimer();
+}
 
 
 var click = function() {  
@@ -38,11 +66,7 @@ var click = function() {
             counter.classList.add('counter');
 
 
-            var interval = setInterval(numbers, 1750);
-
-            if (counter.innerHTML == 'GO !')
-                clearInterval(interval);
-
+            interval = setInterval(numbers, 1750);                
         }
     
 }
@@ -56,11 +80,17 @@ function numbers () {
         console.log(go);
     } else {
         counter.innerHTML = 'GO !'; 
+        clearInterval(interval);
+        countdown( "countdown", 5, 0 );
     }     
 }
 
 play.addEventListener('click',click) ;
 
+
+
+
+// OBJECTS
 
 function get_navigo(){
     var navigoanimate = document.getElementsByClassName('navigo_appear');
@@ -99,3 +129,8 @@ function get_pastabox(){
 }
 
 pastabox.addEventListener("click", get_pastabox);
+
+
+// TIMER
+
+
